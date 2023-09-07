610193bc2dab3c6f7f2bc6bab7a21e46e13d50b4
Илья Снежко <valliv2007@ya.ru>
2023-09-07 16:52:41 +0300

    solve BINOMINAL

--- BINOMIAL.py
+++ BINOMIAL.py
@@ -1,4 +1,4 @@
-"""Create a function that takes a integer number n and returns the formula for 
+"""Create a function that takes a integer number n and returns the formula for
 (a+b)n
   as a string.
 
@@ -9,7 +9,6 @@ formula(-2) --> "1/(a^2+2ab+b^2)"
 formula(3)  --> "a^3+3a^2b+3ab^2+b^3"
 formula(5)  --> "a^5+5a^4b+10a^3b^2+10a^2b^3+5ab^4+b^5"
 
- 
 So the answer would look like so : a^5+5a^4b+10a^3b^2+10a^2b^3+5ab^4+b^5
 
 Important notes :
@@ -21,18 +20,40 @@ a^0 and/or b^0 also don't need be shown instead be a normal person and use 1 sin
 You will need to handle both positive and negative numbers + 0
 You will not be tested for float (only negative integers and whole numbers)
 input n goes from -200 to 200."""
+from math import ceil
+from decimal import Decimal, getcontext
 
-from math import factorial
-
+getcontext().prec = 80
 
 def formula(n):
-    if n < 0:
-        n = abs(n)
-    if not n:
+    if n == 0:
         return "1"
-    fact_n = factorial(n)
-    fact_k = 1
-    fact_nk = 1
+    elif abs(n) == 1:
+        result = 'a+b'
+    elif abs(n) == 2:
+        result = 'a^2+2ab+b^2'
+    else:
+        x = abs(n)
+        multiplier = x
+        result = f'a^{x}'
+        for k in range(1, x):
+            if k == 1:
+                result += f'+{x}a^{x -1}b'
+            elif x-k == 1:
+                result += f'+{x}ab^{x -1}'
+            else:
+                print(k)
+                multiplier = Decimal(multiplier) / Decimal(k)
+                print((x - k + 1))
+                print(multiplier)
+                multiplier = Decimal(multiplier) * Decimal(x - k + 1)
+                multiplier = round(multiplier)
+                print(multiplier)
+                result += f'+{int(multiplier)}a^{x - k}b^{k}'
+        result += f'+b^{x}'
+    if n < 0:
+        result = '1/(' + result + ')'
+    return result
 
-    for k in range(1, n + 1):
-      
+
+print(formula(97))
---
+++ set_join.py
@@ -0,0 +1,8 @@
+set1 = {1, 2, 3, 4, 5}
+set2 = {3, 4, 5, 6, 7}
+result = len(set1 & set2)
+print(result)
+
+list = [[ ]] * 5
+list[0].append(10)
+print(list)
